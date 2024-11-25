3d6cc28a4d056703f577657a26d6ebc6e6dd41fe
Zingke99 <135798795+Zingke99@users.noreply.github.com>
2024-11-25 17:26:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_HASH = os.getenv("API_HASH", "527e6297989f4e7cda5091f5bf41d0e4")
 API_ID = int(os.getenv("API_ID", "10660564"))
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7716239035:AAH_H4NnzFG3QD7VJbw3kC77A8pt1D6VeyY")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "EliteZone_Movies")
 BOT_OWNER = int(os.getenv("BOT_OWNER", "5552521515"))
 DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Koi:Lok@cluster0.bt8r7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
